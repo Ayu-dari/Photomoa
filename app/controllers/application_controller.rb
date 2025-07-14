ff8ea3178138ca7035b4a89e923d3943a64107ba
Ayuchi <ayu.dari0914@gmail.com>
2025-07-14 16:55:02 +0900

    マイページにログアウトリンク追加、UJS対応の調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,8 @@ class ApplicationController < ActionController::Base
   def after_sign_up_path_for(resource)
     mypage_path
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
