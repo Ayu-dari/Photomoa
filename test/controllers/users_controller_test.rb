4bb77a377c206fe04fe6da9b474dce164841d95c
Ayuchi <ayu.dari0914@gmail.com>
2025-07-14 17:24:56 +0900

    users_controller_testの修正

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,26 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = User.create!(email: "test@example.com", password: "password", name: "テストユーザー")
+  end
+
   test "should get mypage" do
-    get users_mypage_url
+    sign_in_as(@user)  # ← ログイン処理
+    get mypage_url
     assert_response :success
   end
+
+  private
+
+  # ログインヘルパー（テスト用）
+  def sign_in_as(user)
+    post user_session_path, params: {
+      user: {
+        email: user.email,
+        password: "password"
+      }
+    }
+  end
 end
+
