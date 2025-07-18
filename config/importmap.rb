de2cbd731bd62939377862e9a519f0af8fadd875
Ayuchi <ayu.dari0914@gmail.com>
2025-07-18 20:26:00 +0900

    修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@popperjs/core", to: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
